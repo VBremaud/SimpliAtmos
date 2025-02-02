29630eb1806eaf7918fc0d5512e9015907562df3
Vincent Bremaud <lovi.bre@hotmail.fr>
2025-02-02 15:36:05 +0100

    ajout test et formattage auto

---
+++ src/SimpliAtmos.jl
@@ -0,0 +1,12 @@
+module SimpliAtmos
+
+# Charger les dépendances ici
+using ClimaCore
+using CUDA
+using LoopVectorization
+using ThreadsX
+
+# Inclure d'autres fichiers si nécessaire
+# include("core/solveurs.jl")
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Test
+using SimpliAtmos  # Charger le module principal
+
+@testset "Tests du modèle 1D" begin
+    @test 1 + 1 == 2  # Test de base pour vérifier que les tests fonctionnent
+end
